72795751d7d074e943557401194f52e548792aa0
陈泽森 <chenzesen@mini1.cn>
2020-03-09 13:45:01 +0800

    feat: 连线 + 结构优化 1. 去掉冗余的结构; 2. 添加了连线响应; 3. 两层 canvas 进行渲染, 提高性能; 4. 添加 example;

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
 import Lock from './lock';
 
+const lockPassword = [1, 5, 9];
 function App() {
   const container = useRef(null);
   const lock = useRef(null);
+  const [isLock, setIsLock] = useState(true);
   useEffect(() => {
     lock.current = new Lock({
       container: container.current,
-      callback: result => console.log(result)
+      callback: result => {
+        if (result.toString() === lockPassword.toString()) {
+          setIsLock(false);
+        }
+      }
     })
-  })
+  }, [])
   return (
-    <div ref={container} className='container'>
-
-    </div>
+    <>
+      <div ref={container} className='container' />
+      <div>{isLock ? 'lock' : 'unlock' }</div>
+    </>
   );
 }
 
--- src/lock/index.js
+++ src/lock/index.js
@@ -5,115 +5,169 @@ class Lock {
     radius = 40,
     callback = result => {}
   } = {}) {
+
     this._container = container;
     this._keyboard = keyborad;
     this._radius = radius;
     this._callback = callback;
+
+    // {[key: string]: number} 坐标和数字键盘的映射表
+    this._numberMap = {};
+
+    // string[], 结果集
+    this._resultCoordinates = [];
     this._mousemove = this._mousemove.bind(this);
-    this._numberAndCircleCenter = {};
-    this._result = [];
+
     this._init();
   }
 
   _init() {
-    const canvas = document.createElement('canvas');
-    canvas.width = this._container.getBoundingClientRect().width;
-    canvas.height = this._container.getBoundingClientRect().height;
-    this._ctx = canvas.getContext('2d');
+    this._container.style.position = 'relative';
 
-    this._canvas = canvas;
-    this._container.appendChild(this._canvas);
+    const bgCanvas = document.createElement('canvas');
+    const drawCanvas = document.createElement('canvas');
 
-    this._drawCircle();
+    this._width = this._container.getBoundingClientRect().width;
+    this._height = this._container.getBoundingClientRect().height;
+
+    bgCanvas.width = drawCanvas.width = this._width;
+    bgCanvas.height = drawCanvas.height = this._height;
+    bgCanvas.style.position = 'absolute';
+    drawCanvas.style.position = 'absolute';
+
+    this._bgCtx = bgCanvas.getContext('2d');
+    this._drawCtx = drawCanvas.getContext('2d');
+
+    this._bgCanvas = bgCanvas;
+    this._drawCanvas = drawCanvas;
+
+    this._container.appendChild(this._bgCanvas);
+    this._container.appendChild(this._drawCanvas);
+
+    this._drawBg();
     this._bindEvent();
   }
 
-  _drawCircle() {
+  _drawBg() {
     const [rows, cols] = this._keyboard;
-    const width = this._canvas.width;
-    const height = this._canvas.height;
+    const width = this._width;
+    const height = this._height;
     const rowOffset = width / (rows + 1);
     const colOffset = height / (cols + 1);
-    this._numberAndCircleCenter = [];
     for (let col = 0; col < cols; col++) {
       for (let row = rows; row > 0; row--) {
         const circleX = rowOffset * row;
         const circleY = colOffset * (col + 1);
-        this._numberAndCircleCenter[`${circleX}${circleY}`] = (6 + row) - (col * 3) - 1;
-        this._ctx.save();
-        this._ctx.beginPath();
-        this._ctx.moveTo(circleX + this._radius, circleY);
-        this._ctx.arc(circleX, circleY, this._radius, 0, Math.PI * 2);
-        this._ctx.stroke();
-        this._ctx.restore();
+        this._numberMap[`${circleX}_${circleY}`] = (6 + row) - (col * 3);
+        this._bgCtx.save();
+        this._bgCtx.beginPath();
+        this._bgCtx.moveTo(circleX + this._radius, circleY);
+        this._bgCtx.arc(circleX, circleY, this._radius, 0, Math.PI * 2);
+        this._bgCtx.stroke();
+        this._bgCtx.restore();
       }
     }
   }
 
-  _highlightCircle(clientX, clientY) {
+  _draw(clientX, clientY) {
     const [rows, cols] = this._keyboard;
-    const width = this._canvas.width;
-    const height = this._canvas.height;
+    const width = this._width;
+    const height = this._height;
     const rowOffset = width / (rows + 1);
     const colOffset = height / (cols + 1);
 
+    // 计算鼠标坐标是否在圆内
     for (let col = 0; col < cols; col++) {
       for (let row = 0; row < rows; row++) {
         const circleX = rowOffset * (row + 1);
         const circleY = colOffset * (col + 1);
         const isInCircle = Math.sqrt(Math.pow(clientX - circleX, 2) + Math.pow(clientY - circleY, 2)) - this._radius < 0;
-        if (isInCircle && !this._result.includes(this._numberAndCircleCenter[`${circleX}${circleY}`])) {
-          this._result.push(this._numberAndCircleCenter[`${circleX}${circleY}`]);
-          this._ctx.save();
-          this._ctx.beginPath();
-          this._ctx.moveTo(circleX + this._radius / 2, circleY);
-          const radialGradient = this._ctx.createRadialGradient(
-            circleX, circleY, 0,
-            circleX, circleY, this._radius / 2,
-          );
-          radialGradient.addColorStop(0, '#000');
-          radialGradient.addColorStop(0.5, '#fff');
-          radialGradient.addColorStop(1, '#000');
-          this._ctx.fillStyle = radialGradient;
-          this._ctx.arc(circleX, circleY, this._radius / 2, 0, Math.PI * 2);
-          this._ctx.fill();
-          this._ctx.restore();
+        if (isInCircle && !this._resultCoordinates.includes(`${circleX}_${circleY}`)) {
+          this._resultCoordinates.push(`${circleX}_${circleY}`);
         }
       }
     }
+
+    this._drawCtx.clearRect(0, 0, this._width, this._height);
+    this._drawLine(clientX, clientY);
+    this._drawCircle();
+  }
+
+  _drawCircle() {
+    this._resultCoordinates.forEach(coordinate => {
+      const [pointX, pointY] = coordinate.split('_');
+      this._drawCtx.save();
+      this._drawCtx.beginPath();
+      this._drawCtx.moveTo(pointX + this._radius / 2, pointY);
+      const radialGradient = this._drawCtx.createRadialGradient(
+        pointX, pointY, 0,
+        pointX, pointY, this._radius / 2,
+      );
+      radialGradient.addColorStop(0, '#000');
+      radialGradient.addColorStop(0.5, '#fff');
+      radialGradient.addColorStop(1, '#000');
+      this._drawCtx.fillStyle = radialGradient;
+      this._drawCtx.arc(pointX, pointY, this._radius / 2, 0, Math.PI * 2);
+      this._drawCtx.fill();
+      this._drawCtx.restore();
+    });
+  }
+
+  _drawLine(clientX, clientY) {
+    if (this._resultCoordinates.length === 0) {
+      return
+    }
+    const [pointX, pointY] = this._resultCoordinates[0].split('_');
+      this._drawCtx.save();
+      this._drawCtx.beginPath();
+      this._drawCtx.moveTo(pointX, pointY);
+      this._resultCoordinates.forEach((coordinate, index) => {
+        if (index === 0) {
+          return;
+        }
+        const [pointX, pointY] = coordinate.split('_');
+        this._drawCtx.lineTo(pointX, pointY);
+      });
+      this._drawCtx.lineTo(clientX, clientY);
+      this._drawCtx.stroke();
+      this._drawCtx.restore();
+  }
+
+  _feedback() {
+    this._drawCanvas.removeEventListener('mousemove', this._mousemove);
+    this._drawCtx.clearRect(0, 0, this._width, this._height);
+    this._callback(this._resultCoordinates.map(coordinate => {
+      const [x, y] = coordinate.split('_');
+      return this._numberMap[`${x}_${y}`];
+    }));
+    this._resultCoordinates = [];
   }
 
   _bindEvent() {
-    this._canvas.addEventListener('mousedown', e => {
+
+    this._drawCanvas.addEventListener('mousedown', e => {
       const { clientX, clientY } = e;
-      this._highlightCircle(clientX, clientY);
+      this._draw(clientX, clientY);
       this._isMouseDown = true;
-      this._canvas.addEventListener('mousemove', this._mousemove);
+      this._drawCanvas.addEventListener('mousemove', this._mousemove);
     })
 
-    this._canvas.addEventListener('mouseup', e => {
-      this._canvas.removeEventListener('mousemove', this._mousemove);
+    this._drawCanvas.addEventListener('mouseup', () => {
       this._isMouseDown = false;
-      this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
-      this._drawCircle();
-      this._callback(this._result);
-      this._result = [];
+      this._feedback();
     })
 
-    this._canvas.addEventListener('mouseleave', e => {
+    this._drawCanvas.addEventListener('mouseleave', () => {
       if (this._isMouseDown) {
-        this._canvas.removeEventListener('mousemove', this._mousemove);
         this._isMouseDown = false;
-        this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
-        this._drawCircle();
-        this._callback(this._result);
-        this._result = [];
+        this._feedback();
       }
     })
   }
+
   _mousemove(e) {
     const { clientX, clientY } = e;
-    this._highlightCircle(clientX, clientY);
+    this._draw(clientX, clientY);
   }
 }
 
